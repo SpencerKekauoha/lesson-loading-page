565105137cc89e10f0553d25e3c27bd4643dc0ef
Spencer Kekauoha <spkekauoha@gmail.com>
2016-07-19 08:00:21 -0600

    style changes

--- loading.js
+++ loading.js
@@ -3,3 +3,7 @@ window.addEventListener('load', function(){
   var loadScreen = document.getElementById('load-screen');
   document.body.removeChild(loadScreen);
 })
+
+//listening for load event.
+//on load, function will look for an id of load-screen
+//and remove #load-screen and anything nested within it.
